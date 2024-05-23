a57b7a5b4c50eb6a76d695065870dbcf813ccb0f
Cloue Macz <maczyepez@gmail.com>
2024-05-23 16:09:31 -0700

    Added: Add Style Popular Images

--- my-app/src/components/popular.js
+++ my-app/src/components/popular.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { useGlobalContext } from '../context/global';
 
 function Popular(){
@@ -11,19 +12,49 @@ const conditionalRender = () => {
             return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
                 <img src={anime.images.jpg.large_image_url} alt={anime.title}/>
                 <h3>{anime.title}</h3>
-                <p>{anime.synopsis}</p>
             </Link>
         })
     }
 }
 
     return(
-        <div>
-            <div className='popular-anime'>
-                {conditionalRender()}
+        <PopularStyle>
+            <div>
+                <div className='popular-anime'>
+                    {conditionalRender()}
+                </div>
             </div>
-        </div>
+        </PopularStyle>
     )
 }
+
+const PopularStyle = styled.div`
+    dispaly: flex;
+    .popular-anime {
+        margin-top: 2rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        padding-left: 2rem;
+        padding-right: 0;
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+        grid-gap: 4rem;
+        background-color: #fff;
+        border-top: 5px solid #e5e7eb;
+        a{
+            height: 500px;
+            border-radius: 7px
+            border: 5px solid #e5e7eb;
+
+        }
+        a img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 7px;
+        }
+    }
+`;
 
 export default Popular;
