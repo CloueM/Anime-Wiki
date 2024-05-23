4f8a53a164fd2c07da4223667044c75af81825e5
Cloue Macz <maczyepez@gmail.com>
2024-05-23 16:41:24 -0700

    Update: Fetch Anime by ID

--- my-app/src/components/AnimeItem.js
+++ my-app/src/components/AnimeItem.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function AnimeItem() {
     const { id } = useParams()
-    console.log(id)
-    return (
-        <div>AnimeItem</div>
-    )
+    
+    // State
+    const [anime, setAnime] = React.useState({})
+    const [character, setCharacter] = React.useState([])
+    const [showMore, setShowMore] = React.useState(false)
+
+    //Get anime based on ID
+    const getAnime = async (anime) => {
+        const response = await fetch(`https://api.jikan.moe/v4/anime/${anime}`)
+        const data = await response.json()
+        setAnime(data.data)
+        console.log(data.data)
+    }
+
+    //Initial Render
+    useEffect(() =>{
+        getAnime(id)
+    }, [])
 }
 
 export default AnimeItem
