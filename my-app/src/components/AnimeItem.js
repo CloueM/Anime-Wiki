1f36378e696cb43f92b19dd04f7ea13ac9079236
Cloue Macs <maczyepez@gmail.com>
2024-05-24 03:23:23 -0400

    Update: Add Characters

--- my-app/src/components/AnimeItem.js
+++ my-app/src/components/AnimeItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 function AnimeItem() {
@@ -7,7 +7,7 @@ function AnimeItem() {
     
     // State
     const [anime, setAnime] = React.useState({})
-    const [character, setCharacter] = React.useState([])
+    const [characters, setCharacter] = React.useState([])
     const [showMore, setShowMore] = React.useState(false)
 
     //Destructure Specific Anime
@@ -35,7 +35,6 @@ function AnimeItem() {
         const response = await fetch(`https://api.jikan.moe/v4/anime/${anime}`)
         const data = await response.json()
         setAnime(data.data)
-        console.log(data.data)
     }
 
 
@@ -43,7 +42,7 @@ function AnimeItem() {
     const getCharacters = async (anime) => {
         const response = await fetch(`https://api.jikan.moe/v4/anime/${anime}/characters`)
         const data = await response.json()
-        setCharacter(data.data.characters)
+        setCharacter(data.data)
     }
 
     //Initial Render
@@ -94,24 +93,42 @@ function AnimeItem() {
                 }
 
             </div>
+            <h3 className="title">Characters</h3>
+            <div className="characters">
+                {characters?.map((character, index) => {
+                    const {role} = character;
+                    const {images, name, mal_id} = character.character
+                    return <Link to={`/character/${mal_id}`} key={index}>
+                        <div className='character'>
+                            <img src={images?.jpg.image_url} alt='' />
+                            <h4>{name}</h4>
+                            <p>{role}</p>
+                        </div>
+                    </Link>
+                })}
+            </div>
         </AnimeItemStyled>
     )
 }
 
 const AnimeItemStyled = styled.div`
     padding: 3rem 18rem;
-    background-color: #EDEDED;
+    background-color: #303841;
 
     h1, .title{
         font-size: 3rem;
         font-weight: 600;
         margin-bottom: 2rem;
+        background: linear-gradient(109.6deg, rgb(83, 231, 173) 11.2%, rgb(67, 209, 222) 100.2%);;
+        cursor: pointer;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
     }
     
     
     .anime_description{
         margin-top: 2rem;
-        color: #6c7983;
+        color: #eeeeee;
         line-height: 1.7rem;
         button{
             background-color: transparent;
@@ -119,16 +136,16 @@ const AnimeItemStyled = styled.div`
             outline: none;
             cursor: pointer;
             font-size: 1.2rem;
-            color: #27AE60;
+            color: rgb(83, 231, 173);
             font-weight: 600;
         }
     }
 
     .details{
-        background-color: white;
+        background-color: #3a4750;
         border-radius: 20px;
         padding: 2rem;
-        border: 5px solid #e5e7eb;
+        border: 5px solid #3a4750;
         .detail{
             display: grid;
             grid-template-columns: repeat(2, 1fr);
@@ -148,7 +165,40 @@ const AnimeItemStyled = styled.div`
 
             p span:first-child{
                 font-weight: bold;
-                color: #6c7983;
+                color: #9bf4d5;
+            }
+        }
+    }
+
+    .characters{
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        grid-gap: 2rem;
+        background-color: #3a4750;
+        border-radius: 20px;
+        padding: 2rem;
+        border: 5px solid #3a4750;
+        .character{
+            padding: .4rem .6rem;
+            border-radius: 10px;
+            background-color: #303841;
+            transition: all 0.5s ease-in-out;
+            img{
+                width: 100%;
+                border-radius: 7px
+            }
+            
+            h4{
+                padding:.5rem 0;
+                color: #9bf4d5;
+            }
+            p{
+                padding:.3rem 0;
+                color: #d9dad7;
+            }
+    
+            &:hover{
+                transform: translateY(-5px);
             }
         }
     }
